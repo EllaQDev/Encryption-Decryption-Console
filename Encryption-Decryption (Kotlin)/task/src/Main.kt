5ee95170a56393fb3a78ba263f3a594f50378208
Ella_Q_Kotlin <baellap8@gmail.com>
2024-06-27 18:30:36 -0700

    ** stage 2 for Encrypt-Decrypt Project via Hyperskill ** creates CaesarCipher class, allowing encryption based on a letter passed to the ctor

--- Encryption-Decryption (Kotlin)/task/src/Main.kt
+++ Encryption-Decryption (Kotlin)/task/src/Main.kt
@@ -1,14 +1,45 @@
 package encryptdecrypt
 
 fun main() {
-    val input = "we found a treasure"
+//    val input = "we found a treasure"
+//    val alpha = "abcdefghijklmnopqrstuvwxyz"
+//    val reversedAlpha = alpha.reversed()
+//    val sb = StringBuilder()
+//    for (c in input) {
+//        if (c != ' '){
+//            sb.append(reversedAlpha[alpha.indexOf(c)])
+//        } else sb.append(" ")
+//    }
+//    println("${sb.toString()}!")
     val alpha = "abcdefghijklmnopqrstuvwxyz"
-    val reversedAlpha = alpha.reversed()
-    val sb = StringBuilder()
-    for (c in input) {
-        if (c != ' '){
-            sb.append(reversedAlpha[alpha.indexOf(c)])
-        } else sb.append(" ")
+    val phraseInput = readln()
+    val keyNum = readln().toInt()
+    val charKey = alpha[keyNum - 1]
+    val cc = CaesarCipher(charKey)
+    println(cc.encryptMessage(phraseInput))
+}
+
+class CaesarCipher(val key: Char) {
+    val alpha = "abcdefghijklmnopqrstuvwxyz"
+    var cipherAlpha : String? = null
+    init {
+        cipherAlpha = alpha.substringAfter(key) + alpha.substringBefore(key)
     }
-    println("${sb.toString()}!")
+
+    fun encryptMessage(msg: String): String {
+        val sb = StringBuilder()
+        for (c in msg) {
+            sb.append(encryptChar(c))
+        }
+        return sb.toString()
+    }
+
+    fun encryptChar(ch: Char) : Char {
+        if (ch.isLetter()) {
+            val indAlpha = alpha.indexOf(ch)
+            return cipherAlpha!![indAlpha]
+        } else {
+            return ch
+        }
+    }
 }
